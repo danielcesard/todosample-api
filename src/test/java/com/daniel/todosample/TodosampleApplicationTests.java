3eede512788808a60a2c662766e92c9e7667781c
danielcesard <danielcesard@gmail.com>
2024-10-13 20:23:31 -0300

    String boot API

---
+++ src/test/java/com/daniel/todosample/TodosampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.todosample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
