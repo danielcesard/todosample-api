23930e147c10e312fb5f57c1f4052bcf010db98c
danielcesard <danielcesard@gmail.com>
2024-10-30 20:32:50 -0300

    Create TaskController

--- src/main/java/com/daniel/todosample/services/TasckService.java
+++ src/main/java/com/daniel/todosample/services/TasckService.java
@@ -1,5 +1,6 @@
 package com.daniel.todosample.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,11 @@ public class TasckService {
         return task.orElseThrow(
             () -> new RuntimeException("Tarefa não encontrada. Id: " + id + ", Tipo: " + Task.class.getName())
         );
+    }
+
+    public List<Task> findAllByUserId(Long userId) {
+        List<Task> tasks = this.taskRepository.findByUser_Id(userId);
+        return tasks;        
     }
 
     @Transactional
