bfb97d2fba6850d1a51d57588de1983aa67a6cd5
danielcesard <danielcesard@gmail.com>
2024-12-11 21:49:24 -0300

    Create UserDetailsService and JWTUtil

--- src/main/java/com/daniel/todosample/configs/SecurityConfig.java
+++ src/main/java/com/daniel/todosample/configs/SecurityConfig.java
@@ -2,24 +2,38 @@ package com.daniel.todosample.configs;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.daniel.todosample.securty.JWTUtil;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 
 public class SecurityConfig {
+
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JWTUtil jwtUtil;
 
     private static final String[] PUBLIC_MATCHERS = {
         "/"
@@ -35,10 +49,18 @@ public class SecurityConfig {
             
         http.cors().and().csrf().disable();
 
+        AuthenticationManagerBuilder authenticationManagerBuilder = http
+            .getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder.userDetailsService(this.userDetailsService)
+            .passwordEncoder(bCryptPasswordEncoder());
+
+        this.authenticationManager = authenticationManagerBuilder.build();
+
         http.authorizeRequests()
-        .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
-        .antMatchers(PUBLIC_MATCHERS).permitAll()
-        .anyRequest().authenticated();
+            .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+            .antMatchers(PUBLIC_MATCHERS).permitAll()
+            .anyRequest().authenticated();
         
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         
