5dfe267577b55ff90af0ec05c8e6bd8a1d75fe7a
danielcesard <danielcesard@gmail.com>
2024-12-15 11:58:14 -0300

    Create JWTAuthenticationFilter

--- src/main/java/com/daniel/todosample/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/daniel/todosample/exceptions/GlobalExceptionHandler.java
@@ -10,8 +10,12 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import com.daniel.todosample.services.exceptions.ObjectNotFoundException;
 import com.daniel.todosample.services.exceptions.DataBindingViolationException;
 
+import java.io.IOException;
 import java.net.http.HttpHeaders;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolationException;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -19,13 +23,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.validation.FieldError;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j(topic = "GLOBAL_EXCEPTION_HANDLER")
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler implements AuthenticationFailureHandler {
 
     @Value("${server.error.include-exception}")
     private boolean printStackTrace;
@@ -145,5 +151,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
         }
 
         return ResponseEntity.status(httpStatus).body(errorResponse);
+    }
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException exception) throws IOException, ServletException {
+
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType("Application/json");
+
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.FORBIDDEN.value(), "Email ou senha inválidos.");
+        response.getWriter().append(errorResponse.toJson());
     }
 }
